53361ca700a6283bed317d55d3ac7bf8a950c1f8
meghanareddybolla <bollameghanareddy@gmail.com>
2023-02-03 14:28:43 -0600

    Add project files.

---
+++ Srinu.Cloud.Test.Api/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Srinu.Cloud.Test.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet(Name = "pubsubdata")]
+        public async Task<IActionResult> pubsubdata(HttpContext httpContext )
+        { 
+        
+            HttpResponse response = httpContext.Response;
+
+            _logger.LogInformation("function got called");
+            _logger.LogInformation(httpContext?.Request?.Body?.ToString());
+            _logger.LogInformation("function call ended");
+            return Ok(response);
+
+        }
+    }
+}
